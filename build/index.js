c9bf7c89696534d0fa83a3bf3eb3cb48ace69f52
Hassan Mehmood <hassanafridi1913@gmail.com>
2023-01-01 16:30:40 +0500

    api version complete

--- build/index.js
+++ build/index.js
@@ -1,48 +1,48 @@
 #! /usr/bin/env node
-import inquirer from "inquirer";
-import * as dotenv from "dotenv";
+import inquirer from 'inquirer';
+import * as dotenv from 'dotenv';
 dotenv.config();
 const CURRENCIES = [
-    { name: "United States Doller (USD)", code: "USD" },
-    { name: "Pakistani Rupee (PKR)", code: "PKR" },
-    { name: "Indian Rupee (INR)", code: "INR" },
-    { name: "Great British Pound (GBP)", code: "GBP" },
-    { name: "Japanese Yen (JPY)", code: "JPY" },
-    { name: "Chinese Yen (CNY)", code: "CNY" },
-    { name: "Euro (EUR)", code: "EUR" },
-    { name: "United Arab Emirates Dirham (AED)", code: "AED" },
+    { name: 'United States Doller (USD)', code: 'USD', rate: 1 },
+    { name: 'Pakistani Rupee (PKR)', code: 'PKR', rate: 226.56 },
+    { name: 'Indian Rupee (INR)', code: 'INR', rate: 82.74 },
+    { name: 'Great British Pound (GBP)', code: 'GBP', rate: 0.8285 },
+    { name: 'Japanese Yen (JPY)', code: 'JPY', rate: 131.56 },
+    { name: 'Chinese Yen (CNY)', code: 'CNY', rate: 6.9181 },
+    { name: 'Euro (EUR)', code: 'EUR', rate: 0.9358 },
+    { name: 'United Arab Emirates Dirham (AED)', code: 'AED', rate: 3.6725 },
 ];
 async function main() {
     const { fromCurrency, amount, toCurrency } = await inquirer.prompt([
         {
-            message: "Select the currency",
-            name: "fromCurrency",
+            message: 'Select the currency',
+            name: 'fromCurrency',
             choices: CURRENCIES,
-            type: "list",
+            type: 'list',
         },
         {
-            message: "Enter the amount",
-            name: "amount",
-            type: "input",
+            message: 'Enter the amount',
+            name: 'amount',
+            type: 'input',
             validate: (value) => {
                 if (isNaN(value) || value.length === 0) {
-                    return "Please enter a number";
+                    return 'Please enter a number';
                 }
                 return true;
             },
         },
         {
-            message: "Select the currency you want to convert",
-            name: "toCurrency",
+            message: 'Select the currency you want to convert',
+            name: 'toCurrency',
             choices: CURRENCIES,
-            type: "list",
+            type: 'list',
         },
     ]);
     convertCurrency(fromCurrency, amount, toCurrency);
 }
 async function convertCurrency(fromCurrency, amount, toCurrency) {
-    let fromCurrencyCode = "";
-    let toCurrencyCode = "";
+    let fromCurrencyCode = '';
+    let toCurrencyCode = '';
     for (let i = 0; i < CURRENCIES.length; i++) {
         if (CURRENCIES[i].name === fromCurrency)
             fromCurrencyCode = CURRENCIES[i].code;
@@ -50,8 +50,8 @@ async function convertCurrency(fromCurrency, amount, toCurrency) {
             toCurrencyCode = CURRENCIES[i].code;
     }
     try {
-        const apiResponse = fetch(`${process.env.API_KEY}/${fromCurrencyCode}/${toCurrencyCode}`);
-        const data = await (await apiResponse).json();
+        const apiResponse = await fetch(`${process.env.API_KEY}/${fromCurrencyCode}/${toCurrencyCode}`);
+        const data = await apiResponse.json();
         const exChangeCurrency = (data.conversion_rate * amount).toFixed(2);
         console.log(`${amount} ${fromCurrencyCode} = ${exChangeCurrency} ${toCurrencyCode}`);
     }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
-import inquirer from "inquirer";
-import * as dotenv from "dotenv";
+import inquirer from 'inquirer';
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 interface ApiType {
@@ -17,71 +17,62 @@ interface ApiType {
 }
 
 const CURRENCIES = [
-  { name: "United States Doller (USD)", code: "USD" },
-  { name: "Pakistani Rupee (PKR)", code: "PKR" },
-  { name: "Indian Rupee (INR)", code: "INR" },
-  { name: "Great British Pound (GBP)", code: "GBP" },
-  { name: "Japanese Yen (JPY)", code: "JPY" },
-  { name: "Chinese Yen (CNY)", code: "CNY" },
-  { name: "Euro (EUR)", code: "EUR" },
-  { name: "United Arab Emirates Dirham (AED)", code: "AED" },
+  { name: 'United States Doller (USD)', code: 'USD', rate: 1 },
+  { name: 'Pakistani Rupee (PKR)', code: 'PKR', rate: 226.56 },
+  { name: 'Indian Rupee (INR)', code: 'INR', rate: 82.74 },
+  { name: 'Great British Pound (GBP)', code: 'GBP', rate: 0.8285 },
+  { name: 'Japanese Yen (JPY)', code: 'JPY', rate: 131.56 },
+  { name: 'Chinese Yen (CNY)', code: 'CNY', rate: 6.9181 },
+  { name: 'Euro (EUR)', code: 'EUR', rate: 0.9358 },
+  { name: 'United Arab Emirates Dirham (AED)', code: 'AED', rate: 3.6725 },
 ];
 
 async function main() {
   const { fromCurrency, amount, toCurrency } = await inquirer.prompt([
     {
-      message: "Select the currency",
-      name: "fromCurrency",
+      message: 'Select the currency',
+      name: 'fromCurrency',
       choices: CURRENCIES,
-      type: "list",
+      type: 'list',
     },
     {
-      message: "Enter the amount",
-      name: "amount",
-      type: "input",
+      message: 'Enter the amount',
+      name: 'amount',
+      type: 'input',
       validate: (value) => {
         if (isNaN(value) || value.length === 0) {
-          return "Please enter a number";
+          return 'Please enter a number';
         }
         return true;
       },
     },
     {
-      message: "Select the currency you want to convert",
-      name: "toCurrency",
+      message: 'Select the currency you want to convert',
+      name: 'toCurrency',
       choices: CURRENCIES,
-      type: "list",
+      type: 'list',
     },
   ]);
 
   convertCurrency(fromCurrency, amount, toCurrency);
 }
 
-async function convertCurrency(
-  fromCurrency: string,
-  amount: number,
-  toCurrency: string
-) {
-  let fromCurrencyCode = "";
-  let toCurrencyCode = "";
+async function convertCurrency(fromCurrency: string, amount: number, toCurrency: string) {
+  let fromCurrencyCode = '';
+  let toCurrencyCode = '';
   for (let i = 0; i < CURRENCIES.length; i++) {
-    if (CURRENCIES[i].name === fromCurrency)
-      fromCurrencyCode = CURRENCIES[i].code;
+    if (CURRENCIES[i].name === fromCurrency) fromCurrencyCode = CURRENCIES[i].code;
 
     if (CURRENCIES[i].name === toCurrency) toCurrencyCode = CURRENCIES[i].code;
   }
 
   try {
-    const apiResponse = fetch(
-      `${process.env.API_KEY}/${fromCurrencyCode}/${toCurrencyCode}`
-    );
-    const data: ApiType = await (await apiResponse).json();
+    const apiResponse = await fetch(`${process.env.API_KEY}/${fromCurrencyCode}/${toCurrencyCode}`);
+    const data: ApiType = await apiResponse.json();
 
     const exChangeCurrency = (data.conversion_rate * amount).toFixed(2);
 
-    console.log(
-      `${amount} ${fromCurrencyCode} = ${exChangeCurrency} ${toCurrencyCode}`
-    );
+    console.log(`${amount} ${fromCurrencyCode} = ${exChangeCurrency} ${toCurrencyCode}`);
   } catch (error) {
     console.log(error);
   }
